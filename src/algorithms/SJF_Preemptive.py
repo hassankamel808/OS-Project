99d22a15ea0863f43a176b8fd2f3bcc645482e95
hassankamel808 <hassanaboshady808@gmail.com>
2026-04-14 17:38:25 +0200

    Add files via upload

---
+++ src/algorithms/SJF_Preemptive.py
@@ -0,0 +1,18 @@
+from src.core.scheduler import Scheduler
+from src.models.process import Process
+from typing import Optional
+
+class SJFPreemptiveScheduler(Scheduler):
+    def __init__(self):
+        super().__init__("Shortest Job First (Preemptive)")
+
+    def get_next_process(self, current_time) -> Optional[Process]:
+        ready_processes = self.get_arrived_processes(current_time)
+
+        if not ready_processes:
+            return None
+
+        return sorted(
+            ready_processes,
+            key=lambda p: (p.get_burst_time(), p.get_arrival_time(), p.get_pid())
+        )[0]
---
+++ src/algorithms/SJF_non_Preemptive.py
@@ -0,0 +1,25 @@
+from src.core.scheduler import Scheduler
+from src.models.process import Process
+from typing import Optional
+
+class SJFNonPreemptiveScheduler(Scheduler):
+    def __init__(self):
+        super().__init__("Shortest Job First (Non-Preemptive)")
+        self.current_running_process = None
+
+    def get_next_process(self, current_time) -> Optional[Process]:
+        if self.current_running_process and not self.current_running_process.is_completed():
+            return self.current_running_process
+
+        ready_processes = self.get_arrived_processes(current_time)
+
+        if not ready_processes:
+            self.current_running_process = None
+            return None
+
+        self.current_running_process = sorted(
+            ready_processes,
+            key=lambda p: (p.get_burst_time(), p.get_arrival_time(), p.get_pid())
+        )[0]
+
+        return self.current_running_process
